1cb10b6f8564fffd2d681d8b0420abeeb68e1fb3
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-15 20:32:19 -0400

    feat: add soundcloud api hitter util

---
+++ server/utils/sc.ts
@@ -0,0 +1,46 @@
+import type { H3Event } from 'h3'
+import type { z } from 'zod'
+import { SOUNDCLOUD_API_URL } from '#shared/constants'
+import { Data, Effect } from 'effect'
+import { $fetch } from 'ofetch'
+
+interface Params<T> {
+  event: H3Event
+  endpoint: string
+  options?: Parameters<typeof $fetch>[1]
+  schema: z.ZodType<T>
+}
+
+class ScFetchError extends Data.TaggedError('ScFetchError')<EffectH3Error> {}
+
+export function $sc<T>({ endpoint, event, options, schema }: Params<T>) {
+  return Effect.gen(function* () {
+    const res = yield* Effect.tryPromise({
+      catch: e => new ScFetchError({
+        cause: e,
+        message: `Failed to get data from URL. Did you provide a valid URL?`,
+        statusCode: 500,
+      }),
+      try: async () => $fetch(`${SOUNDCLOUD_API_URL}${endpoint}`, {
+        ...options,
+        headers: {
+          ...options?.headers,
+          'Content-Type': 'application/json',
+        },
+        params: {
+          ...options?.params,
+          client_id: event.context.clientId,
+        },
+      }),
+    })
+
+    return yield* Effect.try({
+      catch: e => new ScFetchError({
+        cause: e,
+        message: `Invalid response from ${endpoint}`,
+        statusCode: 422,
+      }),
+      try: () => schema.parse(res),
+    })
+  })
+}
