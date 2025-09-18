b416b935987dcf167a66e8555dfec50d2551c74f
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-18 16:27:12 -0400

    refactor: edit useTrack composable

--- app/composables/use-track.ts
+++ app/composables/use-track.ts
@@ -1,33 +1,12 @@
-import { ID3Writer } from 'browser-id3-writer'
-import { saveAs } from 'file-saver'
-
 export const useTrack = createSharedComposable(() => {
   const { isPending: isDownloadingTrack, mutate: downloadTrack } = useMutation({
-    mutationFn: async (url: string) => {
-      const [audioMeta, audioStream] = await Promise.all([
-        $fetch<TrackData>('/api/track/meta', {
-          query: {
-            url,
-          },
-        }),
-        $fetch<ReadableStream>('/api/track', {
-          query: {
-            url,
-          },
-          responseType: 'stream',
-        }),
-      ])
-
-      const audioBuffer = await streamToBuffer(audioStream)
-
-      return {
-        audioMeta,
-        blob: await writeId3(audioBuffer, audioMeta),
-      }
-    },
-    onSuccess: ({ audioMeta, blob }) => {
-      saveAs(blob, `${audioMeta.publisher_metadata?.artist ?? audioMeta.user.full_name} - ${audioMeta.title}.mp3`)
-    },
+    mutationFn: async (trackUrl: string) => $fetch('/api/track', {
+      method: 'GET',
+      query: {
+        url: trackUrl,
+      },
+    }),
+    onSuccess: url => downloadFile(url),
   })
 
   return {
@@ -35,49 +14,3 @@ export const useTrack = createSharedComposable(() => {
     isDownloadingTrack,
   }
 })
-
-async function writeId3(audioBuffer: ArrayBufferLike, audioMeta: TrackData) {
-  const writer = new ID3Writer(audioBuffer)
-
-  const tags: Record<string, string | string[] | { description: string, text: string } | null> = {
-    APIC: audioMeta.artwork_url?.replace('large', 'original') ?? audioMeta.user.avatar_url.replace('large', 'original'),
-    COMM: audioMeta.description
-      ? {
-          description: 'Comment',
-          text: audioMeta.description,
-        }
-      : null,
-    TCON: audioMeta.genre ? [audioMeta.genre] : null,
-    TDAT: audioMeta.display_date,
-    TIT2: audioMeta.title,
-    TPE1: [audioMeta.publisher_metadata?.artist ?? audioMeta.user.full_name],
-    WOAS: audioMeta.permalink_url,
-  }
-
-  await Promise.all(Object.entries(tags).map(async ([tag, value]) => {
-    if (value === null) {
-      return
-    }
-
-    if (tag === 'APIC') {
-      const arrayBuffer = await $fetch<ArrayBufferLike>(getOriginalArtworkUrl(value as string), {
-        responseType: 'arrayBuffer',
-      })
-
-      writer.setFrame('APIC', {
-        data: arrayBuffer,
-        description: 'Attached cover',
-        // CoverFront = 0x03
-        type: 0x03,
-      })
-
-      return
-    }
-
-    // @ts-expect-error - browser-id3-writer types are bad, verified in source
-    writer.setFrame(tag, value)
-  }))
-
-  writer.addTag()
-  return writer.getBlob()
-}
