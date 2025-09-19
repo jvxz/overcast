4c9b48e7274edebd1ae49ddb849899b077499d20
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-19 17:36:44 -0400

    feat: add static bg-background class to html element

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        style: 'background-color: var(--color-background);',
+      },
+      title: 'overcast',
+    },
+  },
+
   compatibilityDate: '2025-07-15',
 
   css: ['~/assets/css/globals.css'],
