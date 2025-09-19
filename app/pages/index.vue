20a8019c1123afa4bf8126082a7b729ab1fd7144
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-19 00:30:05 -0400

    feat: create mockup input box

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,52 +1,48 @@
 <script lang="ts" setup>
-const scrollArea = useTemplateRef('scrollArea')
-const url = ref('')
+const trackUrl = ref('')
 
-const tracks = ref<TrackData[]>([])
+const { downloadTrack, isDownloadingTrack } = useTrack()
 
-const nextHref = ref<string | null>(null)
-const { execute: getMoreUserTracks, pending } = useFetch('/api/user/tracks', {
-  immediate: false,
-  onResponse: (data) => {
-    nextHref.value = data.response._data?.next_href ?? null
-    tracks.value = [...tracks.value, ...data.response._data?.collection ?? []]
-  },
-  params: {
-    nextHref,
-    url,
-  },
-  watch: false,
-})
-
-useInfiniteScroll(scrollArea, () => getMoreUserTracks(), {
-  canLoadMore: () => !!nextHref.value,
-  distance: 24,
-})
-
-function handleRequest(newUrl: string) {
-  tracks.value = []
-  nextHref.value = null
-
-  url.value = newUrl
-  getMoreUserTracks()
+function handleSubmit() {
+  if (trackUrl.value) {
+    downloadTrack(trackUrl.value)
+  }
 }
 </script>
 
 <template>
-  <div class="mx-auto flex h-screen w-lg flex-col items-center justify-center gap-4">
-    <UInput
-      v-model="url"
-      :disabled="pending"
-      @keydown.enter="handleRequest($event.target.value)"
-    />
-
-    <div ref="scrollArea" class="h-[300px] w-full overflow-y-auto rounded border">
-      <div
-        v-for="track in tracks"
-        :key="track.id"
-        class="h-20"
-      >
-        <h1>{{ track.title }}</h1>
+  <div class="flex h-screen flex-col items-center justify-center gap-4">
+    <div class="flex w-xl flex-col gap-4">
+      <div class="flex-1" />
+      <UCard class="w-full shrink-0 flex-row gap-2 p-2">
+        <form
+          class="flex w-full items-center gap-3"
+          @submit.prevent="handleSubmit"
+        >
+          <UInput
+            v-model="trackUrl"
+            placeholder="Enter a track URL"
+            class="border-none bg-transparent px-0 pl-3 shadow-none"
+          />
+          <UButton
+            :disabled="!trackUrl"
+            :is-loading="isDownloadingTrack"
+            size="icon"
+            variant="ghost"
+          >
+            <Icon name="tabler:arrow-right" />
+          </UButton>
+        </form>
+      </UCard>
+      <div class="flex-1">
+        <UAlertRoot v-if="trackUrl">
+          <UAlertTitle>
+            Track downloaded successfully
+          </UAlertTitle>
+          <UAlertDescription>
+            The track has been downloaded successfully.
+          </UAlertDescription>
+        </UAlertRoot>
       </div>
     </div>
   </div>
