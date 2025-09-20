b1cabc5097f5c882af3dfbd463390c454c096186
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-20 16:10:16 -0400

    refactor: migrate $sc util

--- server/utils/sc.ts
+++ server/utils/sc.ts
@@ -1,5 +1,4 @@
 import type { z } from 'zod'
-import { Data, Effect, pipe } from 'effect'
 
 interface Params<T> {
   endpoint: string
@@ -8,77 +7,57 @@ interface Params<T> {
   manualClientId?: string
 }
 
-class ScFetchError extends Data.TaggedError('ScFetchError')<EffectH3Error> {}
-class ScUnauthorizedError extends Data.TaggedError('ScUnauthorizedError')<EffectH3Error> {}
-
 /**
  * hit the soundcloud api
  */
-export function $sc<T>({ endpoint, options, schema }: Params<T>) {
-  return Effect.gen(function* () {
-    const program = Effect.gen(function* () {
-      const { getClientId } = useClientId()
+export async function $sc<T>({ endpoint, options, schema }: Params<T>) {
+  const { getClientId } = useClientId()
 
-      const clientId = yield* pipe(
-        Effect.promise(() => getClientId()),
-        Effect.flatMap(clientId => Effect.fromNullable(clientId)),
-        Effect.mapError(e => new ScUnauthorizedError({
-          cause: e,
-          message: `No client ID found. Please try again`,
-          statusCode: 401,
-        })),
-      )
+  const clientId = await getClientId()
 
-      const url = endpoint.startsWith('http') ? endpoint : `${SOUNDCLOUD_API_URL}${endpoint}`
+  if (!clientId) {
+    throw new Error('RETRY')
+  }
 
-      const res = yield* Effect.tryPromise({
-        catch: (e: any) => {
-          if (e.statusCode === 401) {
-            return new ScUnauthorizedError({
-              message: `Unauthorized response from SoundCloud API. Please try again`,
-              statusCode: 401,
-            })
-          }
+  const url = endpoint.startsWith('http') ? endpoint : `${SOUNDCLOUD_API_URL}${endpoint}`
 
-          return new ScFetchError({
-            cause: e,
-            message: `Failed to get data from URL. Did you provide a valid URL?`,
-            statusCode: 500,
-          })
-        },
-        try: async () => $fetch(url, {
-          ...options,
-          headers: {
-            ...options?.headers,
-            'Content-Type': 'application/json',
-          },
-          params: {
-            ...options?.params,
-            client_id: clientId,
-          },
-        }),
-      })
-
-      return yield* Effect.try({
-        catch: e => new ScFetchError({
-          cause: e,
-          message: `Invalid response from ${endpoint}`,
-          statusCode: 422,
-        }),
-        try: () => schema.parse(res),
-      })
-    })
-      .pipe(Effect.catchTag('ScUnauthorizedError', () => Effect.gen(function* () {
-        yield* getFreshClientId()
-
-        return yield* Effect.fail(new ScUnauthorizedError({
-          message: `Unauthorized response from SoundCloud API. Please try again`,
-          statusCode: 401,
-        }))
-      })))
-
-    return yield* Effect.retry(program, {
-      times: 1,
-    })
+  const res = await $fetch(url, {
+    ...options,
+    headers: {
+      ...options?.headers,
+      'Content-Type': 'application/json',
+    },
+    onResponseError: (res) => {
+      switch (res.response.status) {
+        case 401: throw new Error('RETRY')
+        case 404: throw createError({
+          data: res.response._data,
+          statusCode: 404,
+          statusMessage: 'Could not find resource. Did you provide a valid URL?',
+        })
+        default: throw createError({
+          data: res.response._data,
+          message: 'An unexpected error occured while accessing SoundCloud\'s API. Please try again',
+          statusCode: res.response.status,
+          statusMessage: res.response.statusText,
+        })
+      }
+    },
+    params: {
+      ...options?.params,
+      client_id: clientId,
+    },
   })
+
+  const parsed = schema.safeParse(res)
+
+  if (!parsed.success) {
+    throw createError({
+      data: parsed.error,
+      statusCode: 500,
+      statusMessage: 'Failed to parse SoundCloud response',
+    })
+  }
+
+  return parsed.data
 }
