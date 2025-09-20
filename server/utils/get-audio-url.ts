58a5ff1515b091455957403791140b0c41fdfafb
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-20 16:09:53 -0400

    refactor: migrate getAudioUrl server util

--- server/utils/get-audio-url.ts
+++ server/utils/get-audio-url.ts
@@ -1,26 +1,23 @@
-import { Data, Effect, pipe } from 'effect'
-
-class TrackAudioUrlError extends Data.TaggedError('TrackAudioUrlError')<EffectH3Error> {}
-
 export function getAudioUrl(trackData: TrackData) {
-  return pipe(
-    Effect.fromNullable(
-      trackData.media.transcodings.find((transcoding) => {
-        if (transcoding.preset === 'mp3_1_0')
-          return transcoding.url
-        if (transcoding.preset === 'mp3_0_0')
-          return transcoding.url
-        if (transcoding.preset === 'mp3_0_1')
-          return transcoding.url
-        if (transcoding.preset === 'mp3_standard')
-          return transcoding.url
-        return false
-      })?.url,
-    ),
-    Effect.mapError(e => new TrackAudioUrlError({
-      cause: e,
-      message: 'Failed to get audio URL',
+  const url
+    = trackData.media.transcodings.find((transcoding) => {
+      if (transcoding.preset === 'mp3_1_0')
+        return transcoding.url
+      if (transcoding.preset === 'mp3_0_0')
+        return transcoding.url
+      if (transcoding.preset === 'mp3_0_1')
+        return transcoding.url
+      if (transcoding.preset === 'mp3_standard')
+        return transcoding.url
+      return false
+    })?.url
+
+  if (!url) {
+    throw createError({
       statusCode: 500,
-    })),
-  )
+      statusMessage: 'Could not get audio URL from track data',
+    })
+  }
+
+  return url
 }
