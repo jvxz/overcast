27a23c95da6fea49e837a1223ee18d54323f4fce
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-22 20:34:04 -0400

    feat: add aside content component

---
+++ app/components/aside/content.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const { $version } = useNuxtApp()
+const { isAsideOpen } = useAsideState()
+</script>
+
+<template>
+  <div class="flex min-w-[500px] flex-col">
+    <div class="flex h-16 items-center justify-between">
+      <h1 class="text-2xl font-medium">
+        overcast
+      </h1>
+      <div class="flex items-center gap-2">
+        <p class="font-mono text-sm font-medium text-muted-foreground">
+          {{ $version }}
+        </p>
+        <UButton
+          variant="ghost"
+          size="icon"
+          @click="isAsideOpen = false"
+        >
+          <Icon name="mingcute:align-arrow-left-line" class="!size-4" />
+        </UButton>
+      </div>
+    </div>
+  </div>
+</template>
