85fbbddedb3d9c194a06405333c51b14e09e77e8
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-22 22:31:04 -0400

    feat: add aside tabs to aside

--- app/components/aside.vue
+++ app/components/aside.vue
@@ -1,5 +1,23 @@
 <script lang="ts" setup>
-const { isAsideOpen } = useAsideState()
+const { asideTab, isAsideOpen } = useAsideState()
+
+function handleTabClick(tab: typeof ASIDE_TABS[number]) {
+  if (tab === asideTab.value && isAsideOpen.value) {
+    isAsideOpen.value = false
+    return
+  }
+  asideTab.value = tab
+  isAsideOpen.value = true
+}
+
+const tabMap: Record<typeof ASIDE_TABS[number], { icon: string }> = {
+  'artist': {
+    icon: 'mingcute:user-2-line',
+  },
+  'multi-track': {
+    icon: 'mingcute:notebook-2-line',
+  },
+}
 </script>
 
 <template>
@@ -7,13 +25,18 @@ const { isAsideOpen } = useAsideState()
     :data-open="isAsideOpen"
     class="flex w-[var(--aside-width-closed)] shrink-0 gap-3 duration-150 ease-out data-[open=true]:w-[var(--aside-width-open)] motion-reduce:transition-none"
   >
-    <UButton
-      class="size-16"
-      variant="ghost"
-      @click="isAsideOpen = !isAsideOpen"
-    >
-      <Logo class="!size-8" />
-    </UButton>
+    <div class="flex w-16 shrink-0 flex-col gap-1">
+      <UButton
+        v-for="tab in ASIDE_TABS"
+        :key="tab"
+        class="size-16"
+        variant="ghost"
+        @click="handleTabClick(tab)"
+      >
+        <Icon :name="tabMap[tab].icon" class="!size-8" />
+      </UButton>
+    </div>
+
     <Transition>
       <AsideContent v-if="isAsideOpen" />
     </Transition>
