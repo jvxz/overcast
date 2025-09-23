eb4579f17fac19d3d49a8266c3ae32227f2be92d
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-23 15:59:56 -0400

    refactor: refactor useMultiTrack to include functions for adding or removing tracks

--- app/composables/use-multi-track.ts
+++ app/composables/use-multi-track.ts
@@ -1,3 +1,17 @@
-export const useMultiTrack = createGlobalState(() => ({
-  multiTracks: ref<Set<string>>(new Set()),
-}))
+export const useMultiTrack = createGlobalState(() => {
+  const multiTracks = ref<Set<string>>(new Set())
+
+  function addTrackToMultiTrack(trackUrl: string) {
+    multiTracks.value.add(trackUrl)
+  }
+
+  function removeTrackFromMultiTrack(trackUrl: string) {
+    multiTracks.value.delete(trackUrl)
+  }
+
+  return {
+    addTrackToMultiTrack,
+    multiTracks,
+    removeTrackFromMultiTrack,
+  }
+})
