4d226b07507b07c2e12a211f9e310cd9e46d7565
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-23 16:03:05 -0400

    feat: add content for multi track tab in aside content

--- app/components/aside/content/multi-track.vue
+++ app/components/aside/content/multi-track.vue
@@ -1,5 +1,35 @@
+<script lang="ts" setup>
+const { multiTracks } = useMultiTrack()
+
+const hasTracks = computed(() => multiTracks.value.size > 0)
+const placeholder = computed(() => hasTracks.value ? `Search ${multiTracks.value.size} tracks` : 'No tracks added')
+</script>
+
 <template>
-  <div>
-    <h1>Multi-Track</h1>
+  <div class="flex flex-col gap-4">
+    <div class="flex items-center gap-2">
+      <div class="relative w-full">
+        <UInput
+          :disabled="!hasTracks"
+          :placeholder
+          class="pl-8"
+        />
+        <Icon name="mingcute:search-line" class="absolute inset-0 top-1/2 left-3 z-10 !size-3.5 -translate-y-1/2 text-muted-foreground" />
+      </div>
+      <UButton
+        size="icon"
+        variant="soft"
+        :disabled="!hasTracks"
+      >
+        <Icon name="mingcute:delete-line" />
+      </UButton>
+      <UButton
+        size="icon"
+        variant="soft"
+        :disabled="!hasTracks"
+      >
+        <Icon name="mingcute:download-line" />
+      </UButton>
+    </div>
   </div>
 </template>
