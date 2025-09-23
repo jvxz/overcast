c17ce02bff5590605c2afcb95b066a2477911e47
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-22 20:34:17 -0400

    refactor: set default slot to z index 10

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex h-screen gap-3 bg-card p-3">
     <Aside />
-    <main class="h-full flex-1">
+    <main class="h-full flex-1 z-10">
       <slot />
     </main>
   </div>
