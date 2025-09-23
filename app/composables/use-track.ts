c92d278cf631a607de1f18788fd8e2227b92f85d
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-23 18:39:11 -0400

    refactor: extend useTrack composable to force target declaration + use mutation keys

--- app/composables/use-track.ts
+++ app/composables/use-track.ts
@@ -1,13 +1,22 @@
+export const DOWNLOADING_TRACK_KEY = 'downloading-track'
+
 export const useTrack = createSharedComposable(() => {
+  const serverStateTarget = useServerStateTarget()
+
   const { isPending: isDownloadingTrack, mutate: downloadTrack } = useMutation({
-    mutationFn: async (trackUrl: string) => $fetch('/api/track', {
-      method: 'GET',
-      onResponse: handleResponseError,
-      query: {
-        url: trackUrl,
-      },
-      retry: false,
-    }),
+    mutationFn: async ({ target, trackUrl }: { trackUrl: string, target: string }) => {
+      serverStateTarget.value = target
+
+      return $fetch('/api/track', {
+        method: 'GET',
+        onResponse: handleResponseError,
+        query: {
+          url: trackUrl,
+        },
+        retry: false,
+      })
+    },
+    mutationKey: [DOWNLOADING_TRACK_KEY],
     onSuccess: url => downloadFile(url),
   })
 
