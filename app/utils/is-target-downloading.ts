817da781e06631a393abaecbb696699f0537fa37
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-24 14:35:12 -0400

    refactor: remove isTargetDownloading util in favor of isTargetBusy

--- app/utils/is-target-downloading.ts
+++
@@ -1,6 +0,0 @@
-export function isTargetDownloading(target: MaybeRefOrGetter<'index' | string & {}>) {
-  const { isDownloadingTrack } = useMutationState()
-  const serverStateTarget = useServerStateTarget()
-
-  return computed(() => isDownloadingTrack.value && toValue(target) === serverStateTarget.value)
-}
