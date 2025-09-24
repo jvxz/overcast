807437694cad4cc988c74bd584dd1e74577909bd
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-24 14:35:31 -0400

    feat: add multi track downloading to useTrack composable

--- app/composables/use-track.ts
+++ app/composables/use-track.ts
@@ -1,7 +1,9 @@
 export const DOWNLOADING_TRACK_KEY = 'downloading-track'
+export const DOWNLOADING_MULTI_TRACK_KEY = 'downloading-multi-track'
 
 export const useTrack = createSharedComposable(() => {
   const serverStateTarget = useServerStateTarget()
+  const { multiTracks } = useMultiTrack()
 
   const { isPending: isDownloadingTrack, mutate: downloadTrack } = useMutation({
     mutationFn: async ({ target, trackUrl }: { trackUrl: string, target: string }) => {
@@ -20,8 +22,38 @@ export const useTrack = createSharedComposable(() => {
     onSuccess: url => downloadFile(url),
   })
 
+  const { isPending: isDownloadingMultiTracks, mutate: downloadMultiTracks } = useMutation({
+    mutationFn: async () => {
+      serverStateTarget.value = 'multi-track'
+
+      const stream = await $fetch<ReadableStream>('/api/track/multi', {
+        method: 'GET',
+        onResponse: handleResponseError,
+        query: {
+          urls: Array.from(multiTracks.value),
+        },
+        responseType: 'stream',
+      })
+
+      const blob = await streamToBlob(stream)
+      const url = URL.createObjectURL(blob)
+
+      return { filename: `overcast-${Date.now()}.zip`, url }
+    },
+    mutationKey: [DOWNLOADING_MULTI_TRACK_KEY],
+    onSuccess: ({ filename, url }) => downloadFile(url, filename),
+  })
+
+  const isMutating = useIsMutating()
+  const isFetching = useIsFetching()
+
+  const isBusy = computed<boolean>(() => isMutating.value > 0 || isFetching.value > 0)
+
   return {
+    downloadMultiTracks,
     downloadTrack,
+    isBusy,
+    isDownloadingMultiTracks,
     isDownloadingTrack,
   }
 })
