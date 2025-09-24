e2841143fed25b8976f70367130319be2789a6d7
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-24 14:30:30 -0400

    feat: add "zipping" as a server state event

--- shared/utils/constants.ts
+++ shared/utils/constants.ts
@@ -2,7 +2,7 @@ export const SOUNDCLOUD_API_URL = 'https://api-v2.soundcloud.com'
 export const PRESIGNED_URL_EXPIRATION = 60 * 60 * 10
 
 export type ServerState = typeof SERVER_STATE_EVENTS[number]
-export const SERVER_STATE_EVENTS = ['downloading', 'idle', 'uploading'] as const
+export const SERVER_STATE_EVENTS = ['downloading', 'idle', 'uploading', 'zipping'] as const
 
 export type SubmitMode = typeof SUBMIT_MODES[number]
 export const SUBMIT_MODES = ['track', 'artist', 'playlist', 'multi'] as const
