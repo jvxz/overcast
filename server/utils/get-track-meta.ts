fe4c1752e968b2c999e4ef0a2cc5578967043dfa
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-26 22:40:24 -0400

    feat: add support for id arrays for getTrackMeta server util

--- server/utils/get-track-meta.ts
+++ server/utils/get-track-meta.ts
@@ -1,13 +1,31 @@
-export async function getTrackMeta(url: string) {
+export async function getTrackMeta(input: string): Promise<TrackData>
+export async function getTrackMeta(input: number[]): Promise<TrackData[]>
+
+export async function getTrackMeta(input: string | number[]): Promise<TrackData | TrackData[]> {
+  if (typeof input === 'string') {
+    return $sc({
+      endpoint: '/resolve',
+      options: {
+        params: {
+          url: input,
+        },
+        retry: false,
+      },
+      schema: TrackSchema,
+      type: 'track',
+    })
+  }
+
   return $sc({
-    endpoint: '/resolve',
+    endpoint: '/tracks',
     options: {
       params: {
-        url,
+        ids: input.join(','),
+        linked_partitioning: true,
       },
       retry: false,
     },
-    schema: TrackSchema,
+    schema: z.array(TrackSchema),
     type: 'track',
   })
 }
