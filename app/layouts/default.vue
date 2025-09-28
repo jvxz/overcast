8754f8ad56bb9dd0fc7bb51941602270cd90dedf
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-28 16:33:21 -0400

    feat: create basic responsiveness layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="flex h-screen gap-3 bg-card p-3">
+  <div class="hidden h-screen gap-3 bg-card p-3 xl:flex">
     <Aside />
-    <main class="h-full flex-1 z-10">
-      <slot />
+    <main class="z-10 h-full flex-1">
+      <UCard class="flex h-full flex-col items-center justify-center gap-4 bg-background">
+        <slot />
+      </UCard>
+    </main>
+  </div>
+  <div class="flex h-screen gap-3 bg-card p-3 xl:hidden">
+    <main class="z-10 h-full flex-1">
+      <UCard class="flex h-full flex-col items-center justify-center gap-4 bg-background">
+        <slot />
+      </UCard>
     </main>
   </div>
 </template>
