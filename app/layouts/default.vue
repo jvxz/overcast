48e2f7d4b2efba584d0f491695d468795284dc21
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-28 23:41:19 -0400

    refactor: edit default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,15 +1,42 @@
+<script lang="ts" setup>
+import { breakpointsTailwind } from '@vueuse/core'
+
+const asideState = useAsideState()
+const breakpoints = useBreakpoints(breakpointsTailwind)
+
+const isDesktop = breakpoints.xl
+</script>
+
 <template>
-  <div class="hidden h-screen gap-3 bg-card p-3 xl:flex">
-    <Aside />
+  <div class="relative flex h-screen gap-3 xl:bg-card xl:p-3">
+    <aside
+      :data-open="asideState !== null"
+      class="flex w-0 xl:w-[var(--aside-width-closed)] shrink-0 gap-3 duration-150 ease-out xl:data-[open=true]:w-[var(--aside-width-open)] motion-reduce:transition-none"
+    >
+      <Aside v-if="isDesktop" class="hidden xl:flex" />
+      <AsideMobile v-else class="flex xl:hidden" />
+    </aside>
     <main class="z-10 h-full flex-1">
-      <UCard class="flex h-full flex-col items-center justify-center gap-4 bg-background">
-        <slot />
-      </UCard>
-    </main>
-  </div>
-  <div class="flex h-screen gap-3 bg-card p-3 xl:hidden">
-    <main class="z-10 h-full flex-1">
-      <UCard class="flex h-full flex-col items-center justify-center gap-4 bg-background">
+      <UButton
+        class="absolute top-4 left-4 size-12 xl:hidden"
+        size="icon"
+        variant="soft"
+        @click="asideState === 'multi-track' ? asideState = null : asideState = 'multi-track'"
+      >
+        <Icon name="mingcute:align-arrow-right-line" class="!size-7" />
+      </UButton>
+      <UButton
+        class="absolute top-4 right-4 size-12 xl:hidden"
+        size="icon"
+        variant="soft"
+        @click="$colorMode.value === 'dark' ? $colorMode.value = 'light' : $colorMode.value = 'dark'"
+      >
+        <Icon :name="$colorMode.value === 'dark' ? 'mingcute:moon-line' : 'mingcute:sun-line'" class="!size-7" />
+      </UButton>
+      <UCard class="flex h-full flex-col items-center justify-center gap-4 border-none bg-background xl:border-solid">
+        <Transition>
+          <div v-if="asideState !== null" class="absolute inset-0 z-10 block size-full bg-black opacity-50 transition-opacity duration-150 xl:hidden" />
+        </Transition>
         <slot />
       </UCard>
     </main>
