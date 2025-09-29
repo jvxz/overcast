2f18aa363d00205e1993760d160bf1994cdef417
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-09-28 23:40:21 -0400

    refactor: edit progress bar component

--- app/components/progress-bar.vue
+++ app/components/progress-bar.vue
@@ -6,8 +6,8 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div v-bind="$attrs" :class="cn('relative', props.class)">
+  <div v-bind="$attrs" :class="cn('relative overflow-hidden', props.class)">
     <slot />
-    <div class="absolute inset-0 z-100 h-full bg-muted/50 p-0 mix-blend-screen duration-100" :style="{ width: `${progress}%` }"></div>
+    <div class="pointer-events-none absolute inset-0 z-100 h-full bg-black/10 p-0 mix-blend-hard-light duration-100 dark:bg-neutral-700/25 dark:mix-blend-screen" :style="{ width: `${progress}%` }"></div>
   </div>
 </template>
